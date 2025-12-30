15aa7c2df88d53fdc873254b815a5585942160ee
IreGambara <75572639+IreGambara@users.noreply.github.com>
2025-12-30 18:31:56 +0100

    First commit - SubTrack dashboard project

---
+++ main.js
@@ -0,0 +1,70 @@
+/*Active switching - Sidebar */
+const navLinks = document.querySelectorAll('.sidebar .nav-link');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+
+    document
+      .querySelector('.sidebar .nav-link.active')
+      ?.classList.remove('active');
+
+    link.classList.add('active');
+  });
+});
+
+//Current Date
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+const date = new Date();
+
+let month = months[date.getMonth()];
+let day = String(date.getDate()).padStart(2, '0');
+let year = date.getFullYear();
+
+let currentDate = `${month} ${day}, ${year}`;
+document.getElementById("date").innerHTML = currentDate;
+
+//Sidebar
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('sidebar-container');
+  if (!container) return;
+
+  fetch('../pages/sidebar.html')
+    .then(res => {
+      if (!res.ok) throw new Error('Sidebar not found');
+      return res.text();
+    })
+    .then(html => {
+      container.innerHTML = html;
+
+      const currentPage = document.body.dataset.page;
+      const activeLink = container.querySelector(
+        `.nav-link[data-page="${currentPage}"]`
+      );
+
+      if (activeLink) {
+        activeLink.classList.add('active');
+      }
+    })
+    .catch(err => console.error(err));
+});
+
+/* Progress Bar Animation */
+ document.addEventListener("DOMContentLoaded", () => {
+    const groups = document.querySelectorAll("#expensesChart .month-group");
+
+    const values = Array.from(groups).map(
+      group => Number(group.dataset.value)
+    );
+
+    const maxValue = Math.max(...values);
+
+    groups.forEach(group => {
+      const value = Number(group.dataset.value);
+      const bar = group.querySelector(".bar");
+
+      const percentage = (value / maxValue) * 100;
+      bar.style.height = `${percentage}%`;
+    });
+  });
